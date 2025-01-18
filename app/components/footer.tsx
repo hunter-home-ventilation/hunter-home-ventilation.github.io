6bd180c1d9de0de4947c69da02fea876fb9b4e73
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-01-18 22:20:35 +0000

    fixing link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,13 +3,17 @@ import { RiFacebookFill } from '@remixicon/react';
 export function Root() {
   return (
     <div className="bg-secondary-hover">
-      <div className="px-10 py-6 container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex items-center justify-between px-10 py-6">
         <span className="text-secondary text-[13px]">
           © {new Date().getFullYear()} Hunter Home Ventilation. All rights reserved.
         </span>
 
         <div className="flex gap-x-4">
-          <a href="#" className="text-secondary hover:text-primary">
+          <a
+            href="https://www.facebook.com/hunterhomeventilation"
+            target="_blank"
+            className="text-secondary hover:text-primary"
+          >
             <RiFacebookFill className="size-4" />
           </a>
         </div>
